50d28c330932802b1a007a0615fdcfd2387d6c15
ShioriItai <siycha@gmail.com>
2023-06-22 01:52:33 +0900

    2023/06/21

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,6 +49,15 @@ $('.hamburgerMenu-guide-icon').hover(function() {
 	$(this).children('p').toggleClass('hover');
 });
 
+//虫眼鏡
+$('.header-search-icon').click(function() {
+	$('.searchMenu').toggleClass('active');
+});
+
+$('.searchMenu-close-btn,.hamburgerMenu-sp-search').click(function() {
+	$('.searchMenu').toggleClass('active');
+});
+
 //コンテンツ
 $('.contents-text-view').hover(function() {
 	$(this).children().toggleClass('hover');
@@ -71,4 +80,31 @@ $('.footer-main-pc ul li p').hover(function() {
 
 $('.footer-guide-icon').hover(function() {
 	$(this).children('p').toggleClass('hover');
+});
+
+//slider
+$('.slider').slick({
+	autoplay: false,//自動的に動き出すか。初期値はfalse。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 3,//スライドを画面に3枚見せる
+	slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	dots: true,//下部ドットナビゲーションの表示
+	responsive: [
+		{
+		breakpoint: 2000,//モニターの横幅が769px以下の見せ方
+		settings: {
+			slidesToShow: 5,//スライドを画面に2枚見せる
+			slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+		}
+	},
+	{
+		breakpoint: 426,//モニターの横幅が426px以下の見せ方
+		settings: {
+			slidesToShow: 2,//スライドを画面に1枚見せる
+			slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+		}
+	}
+]
 });
